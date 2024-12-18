1aab13fefb0b4ec7d7f754987a3331ab49a5dee2
vinay-kumar-3 <vinaykumarkosanam9@gmail.com>
2024-12-18 15:09:44 +0530

    setup

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Your HTML file (Vite's entry point)
+    "./src/**/*.{js,ts,jsx,tsx}", // Your React components and files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
